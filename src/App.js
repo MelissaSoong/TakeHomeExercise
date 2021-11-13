78d9741e11b44586dea2c4930b775b4e721bc237
MelissaSoong <ysoong2@huskers.unl.edu>
2021-11-13 11:09:11 +0800

    Remove unused files.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import Form from './Form';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Temperature from './Temperature';
